d81d29584fc771c4701f846264d9724fa3e75936
LVolkova <wolka90@yandex.ru>
2023-01-19 22:38:27 +0300

    Убирает прокрутку страницы под модальным окном

--- source/js/modal.js
+++ source/js/modal.js
@@ -5,6 +5,7 @@ let docModalToggle = document.querySelector('.doc-modal__toggle');
 let docModalImg = document.querySelector('.doc-modal__img');
 let docLinks = document.querySelectorAll('.documents__button');
 let alts = document.querySelectorAll('.documents__text');
+let body = document.querySelector('.page__body');
 
 docModal.classList.remove('doc-modal--nojs');
 
@@ -15,6 +16,7 @@ docLinks.forEach(link => link.addEventListener('click', function(e) {
   let altText = alts[index].textContent;
 
   docModal.classList.remove('doc-modal--closed');
+  body.classList.add('page__body--noscroll');
   docModalImg.src = link;
   docModalImg.setAttribute('alt', altText);
 }));
@@ -23,14 +25,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   window.addEventListener('click', function(e) {
     const target = e.target
-    if (target.closest('.doc-modal__wrapper')) {
+    if (target.closest('.doc-modal__wrapper') && !target.closest('.doc-modal__img')) {
       docModal.classList.add('doc-modal--closed');
+      body.classList.remove('page__body--noscroll');
     }
   })
 
   document.addEventListener('keyup', function(e) {
     if( e.key === 'Escape' ){
       docModal.classList.add('doc-modal--closed');
+      body.classList.remove('page__body--noscroll');
     }
   })
 });
